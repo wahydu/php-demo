3de3f8f95eb2cdede82d19da99eb418177b03870
codeburster <codeburster@gmail.com>
2019-02-06 04:17:14 +0530

    v5.1 Patched DBase connection Logic

--- src/create.php
+++ src/create.php
@@ -1,4 +1,51 @@
-<?php include "templates/header.php"; ?><h2>Add a user</h2>
+<?php
+
+/**
+ * Use an HTML form to create a new entry in the
+ * users table.
+ *
+ */
+
+
+if (isset($_POST['submit'])) {
+	require "../config.php";
+	require "../common.php";
+
+	try {
+		$connection = new PDO($dsn, $username, $password, $options);
+		
+		$new_user = array(
+			"firstname" => $_POST['firstname'],
+			"lastname"  => $_POST['lastname'],
+			"email"     => $_POST['email'],
+			"age"       => $_POST['age'],
+			"location"  => $_POST['location']
+		);
+
+		$sql = sprintf(
+				"INSERT INTO %s (%s) values (%s)",
+				"users",
+				implode(", ", array_keys($new_user)),
+				":" . implode(", :", array_keys($new_user))
+		);
+		
+		$statement = $connection->prepare($sql);
+		$statement->execute($new_user);
+	} catch(PDOException $error) {
+		echo $sql . "<br>" . $error->getMessage();
+	}
+	
+}
+?>
+
+<?php require "templates/header.php"; ?>
+
+<?php if (isset($_POST['submit']) && $statement) { ?>
+	<blockquote><?php echo $_POST['firstname']; ?> successfully added.</blockquote>
+<?php } ?>
+
+<h2>Add a user</h2>
+
 <form method="post">
 	<label for="firstname">First Name</label>
 	<input type="text" name="firstname" id="firstname">
@@ -14,4 +61,5 @@
 </form>
 
 <a href="index.php">Back to home</a>
-<?php include "templates/footer.php"; ?>
+
+<?php require "templates/footer.php"; ?>
